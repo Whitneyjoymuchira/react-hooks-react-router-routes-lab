8ad45fb7035b442fee3f3af9443ba9aa688a060e
Whitney Muchira <whitneymuchira@gmail.com>
2022-07-24 23:01:38 +0300

    Done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,28 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorinfo = actors.map((actor) => (
+    <div key={actor.name}>
+    <h2>{actor.name}</h2>
+    <ul>
+      {actor.movies.map((genre) => (
+        <li key={genre}>{genre}</li>
+      ))}
+    </ul>
+  </div>
+  )
+)
+return (
+  <div>
+    <h1>Actors Page</h1>
+    {actorinfo}
+  </div>
+)
 }
 
-export default Actors;
+
+  
+
+export default Actors
+  
+
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorinfo = directors.map((director) => (
+    <div key={director.name}>
+    <h2>{director.name}</h2>
+    <ul>
+      {director.movies.map((genre) => (
+        <li key={genre}>{genre}</li>
+      ))}
+    </ul>
+  </div>
+  )
+)
+return (
+  <div>
+    <h1>Directors Page</h1>
+    {directorinfo}
+  </div>
+);
 }
 
 export default Directors;
